3705ac3934376c09030c0c1e5f047bb0d84f0d9e
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-13 06:41:05 +0000

    setting up the routes for the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Home from "./UI/Home";
 import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
+import CreateOrder from "./features/cart/CreateOrder";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/menu", element: <Menu /> },
   { path: "/cart", element: <Cart /> },
-  { path: "/order", element: <Order /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
 ]);
 function App() {
   return <RouterProvider router={router} />;
