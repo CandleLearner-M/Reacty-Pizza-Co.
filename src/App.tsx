836496c40dc977949ff79cf20807c0f8724bea67
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-13 06:28:30 +0000

    setting up the routes for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order", element: <Order /> },
+]);
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
-export default App
+export default App;
